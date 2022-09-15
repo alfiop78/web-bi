c6a5e362972da000f6ffeef02868b6804419b661
Alfio Pietrantuono <alfio.p78@gmail.com>
2022-09-15 11:49:43 +0200

    icona e titolo home page

--- resources/views/web_bi/index.blade.php
+++ resources/views/web_bi/index.blade.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>web-bi | HOME</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="/favicon.png" type="image/png" />
+  <title>Gaia-BI | HOME</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
+  <!-- Styles -->
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body class="antialiased">
-        <h2>HOME PAGE</h2>
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    <a href="{{ route('web_bi.mapping') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Mapping Database</a>
-                </div>
+  <style>
+    body {
+      font-family: 'Nunito', sans-serif;
+    }
+  </style>
+</head>
 
-        </div>
-    </body>
+<body class="antialiased">
+  <h2>HOME PAGE</h2>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+      <a href="{{ route('web_bi.mapping') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Mapping Database</a>
+    </div>
+
+  </div>
+</body>
+
 </html>
