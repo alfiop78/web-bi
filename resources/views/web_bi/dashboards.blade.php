b360ca4293aa6251e52e36e5113c1accc73ad1e5
alfiop78 <alfio.p78@gmail.com>
2023-09-27 18:51:55 +0200

    Creata pagina Dashboard

---
+++ resources/views/web_bi/dashboards.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+  <link rel="icon" href="/favicon.png" type="image/png" />
+  <title>Gaia-BI | Dashboard</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-dialog-responsive.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-layout.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/material-icons.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-drawer-responsive.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-control-responsive.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-list-responsive.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('/css/md-versioning.css') }}" />
+  <script src="{{ asset('/js/Application.js') }}"></script>
+  <script src="{{ asset('/js/lib.js') }}"></script>
+  <script src="{{ asset('/js/WBStorage.js') }}"></script>
+  <script src="{{ asset('/js/WorkBooks.js') }}"></script>
+</head>
+
+<body class="antialiased">
+
+  <main>
+    <div id="drawer">
+
+      <section class="account">
+        <i class="material-icons md-light">person</i>
+      </section>
+
+      <nav>
+        <!-- <a href="{{ url('/mapdb') }}"><i class="material-icons-round md-24">home</i></a> -->
+        <section class="icon-vertical-menu">
+          <a href="{{ url('/') }}"><i class="material-icons-round md-24">home</i></a>
+        </section>
+      </nav>
+
+    </div>
+
+    <header>
+      <div class="nav-button">
+        <!-- codelab-nav-button-->
+        <a href="/" id="arrow-back"><i class="material-icons md-light">close</i></a>
+        <a href="#" id="menu" onclick="App.menu()"><i class="material-icons md-light">menu</i></a>
+      </div>
+
+      <h1 class="title">Sincronizzazione risorse</h1>
+    </header>
+
+    <div id="container">
+
+      <div id="content" class="custom-scrollbar">
+
+        <div id="body" hidden>
+
+          <div class="objects">
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div id="controls">
+        <div id="fabs">
+          <!-- <a href="#" id="previous-step" title="Previous step">logout</a> -->
+          <button id="mdc-logout" class="button dense raised">HOME</button>
+          <div class="spacer"></div>
+          <!-- <button id="mdc-home" class="button dense raised" onclick="location.href='home/'">i miei veicoli</button> -->
+          <!-- <a href="#" id="next-step" title="Next step">i miei veicoli</a> -->
+          <!-- <i class="material-icons md-circle md-right md-24" onclick="">navigate_before</i> -->
+          <!-- <a href="/" id="done" hidden="" title="Codelab complete">Done</a> -->
+        </div>
+      </div>
+
+      <div id="console">
+        <div id="fabsConsole">
+          <i class="material-icons md-18">info</i>
+          <p></p>
+        </div>
+      </div>
+    </div>
+
+  </main>
+  <div class="loader">
+    <svg viewBox="0 0 32 32" width="32" height="32">
+      <circle id="spinner" cx="16" cy="16" r="14" fill="none"></circle>
+    </svg>
+  </div>
+  <script type="text/javascript" src="{{ asset('/js/dashboards.js') }}" async></script>
+</body>
+
+</html>
