e4b9bd3f4697d93b81e107a2284d7693fe6257e2
alfiop78 <alfio.p78@gmail.com>
2023-10-12 11:03:45 +0200

    colonna MLI come filtro ma nascosta nella DataTable

--- public/js/init-dashboards.js
+++ public/js/init-dashboards.js
@@ -182,7 +182,8 @@ var Dashboard = new Dashboards(); // istanza della Classe Dashboards, da inizial
 
     // La funzione group() utilizza, come primo parametro, una DataTable, quindi si può anche impostare
     // facendo riferimento al chartWrapper con la funzione getDataTable(), ChartWrapper.getDataTable().
-    let dataGroup = new google.visualization.data.group(dataTable, [0, 1, 2, 3, 4, 5, 6, 7],
+    /* let dataGroup = new google.visualization.data.group(
+      dataTable, [0, 1, 2, 3, 4, 5, 6, 7],
       [
         // OFFICINA INTERNA (costo_rapporto_6)
         { 'column': 16, 'aggregation': google.visualization.data.sum, 'type': 'number' },
@@ -199,32 +200,54 @@ var Dashboard = new Dashboards(); // istanza della Classe Dashboards, da inizial
         // marginalità
         // { 'column': 28, 'aggregation': google.visualization.data.avg, 'type': 'number' }
       ]
-    );
-    console.log(JSON.parse(dataGroup.toJSON()));
-    debugger;
+    ); */
+    // console.log(JSON.parse(dataGroup.toJSON()));
+    // debugger;
     // Dashboard.view = new google.visualization.DataView(dataGroup);
     // Dashboard.view.setColumns([0, 1, 2, 3, 4, 5, 6, 7, 9]);
     // console.log(JSON.parse(Dashboard.view.toJSON()));
     // debugger;
 
-    // NOTE: La funzione group() commentata (sopra) la ricreo utilizzando il template-sheet json
-
-    // NOTE: le proprietà definite nel ChartWrapper vengono impostate nel template-sheet .json, proprietà "wrapper"
+    // var table1 = new google.visualization.ChartWrapper(Dashboard.sheetSpecs.wrapper);
     var table = new google.visualization.ChartWrapper(Dashboard.sheetSpecs.wrapper);
-    // funzioni di formattazione
-    /* for (const [colIndex, properties] of Object.entries(Dashboard.sheetSpecs.data.formatter)) {
-      switch (properties.format) {
-        case 'currency':
-          currencyFormatter.format(dataTable, +colIndex);
-          break;
-        case 'percent':
-          percFormatter.format(dataTable, +colIndex);
-          break;
-        default:
-          break;
-      }
-    } */
-    // console.log(dataTable);
+    // esempio array di View
+    // table.setView([{ columns: [1, 3, 5, 7, 16] }, { columns: [0, 1, 2, 3] }]);
+    table.setView({ columns: [1, 3, 5, 7, 9, 16] });
+    // table.draw(document.getElementById('chart_div'));
+    // console.log(table.getView());
+    // var table2 = new google.visualization.ChartWrapper(Dashboard.sheetSpecs.wrapper);
+    // table
+    google.visualization.events.addListener(table, 'ready', function() {
+      console.log('ready');
+      let tableRef = new google.visualization.Table(document.getElementById('chart_div'));
+      let tableCloned = table.clone();
+      console.log(table.getDataTable());
+      tableCloned.setView({ columns: [0, 1, 2, 3, 4] });
+      console.log(tableCloned.getDataTable());
+      let dataGroup = new google.visualization.data.group(
+        tableCloned.getDataTable(), [0, 1, 2, 3, 4, 5, 6, 7],
+        [
+          // OFFICINA INTERNA (costo_rapporto_6)
+          { 'column': 16, 'aggregation': google.visualization.data.sum, 'type': 'number' },
+          // RA DIRETTA COSTO (costo_rapporto_2)
+          // { 'column': 17, 'aggregation': google.visualization.data.sum, 'type': 'number' },
+          // RA DIRETTA RICAVO (ricavo_rapporto_2)
+          // { 'column': 18, 'aggregation': google.visualization.data.sum, 'type': 'number' },
+          // % MARG. RA DIRETTA (perc_margine_rapporto_2)
+          // { 'column': 25, 'aggregation': google.visualization.data.avg, 'type': 'number' },
+          // costo ve_cb
+          // { 'column': 26, 'aggregation': google.visualization.data.sum, 'type': 'number' },
+          // ricavo_ve_cb
+          // { 'column': 27, 'aggregation': google.visualization.data.sum, 'type': 'number' },
+          // marginalità
+          // { 'column': 28, 'aggregation': google.visualization.data.avg, 'type': 'number' }
+        ]
+      );
+      // gdashboard.draw(dataGroup);
+      tableRef.draw(dataGroup);
+      // tableCloned.draw(document.getElementById('chart_div'));
+
+    });
 
     // "ubicazione_ds" influenza "marca_veicolo_ds" -> "marca_veicolo_ds" influenza "modello_ds"
     // -> "modello_ds" infleunza "settore_ds" e tutti (l'array Dashboard.controlsWrapper) influenzano la table
